eebbf3209b782599c8c4de01e6a3e121f0ccbe32
Paolo Dolci <pdolci@gmail.com>
2026-02-24 07:33:56 +0100

    Merge alembic heads

---
+++ migrations/versions/7d522e70629f_merge_heads.py
@@ -0,0 +1,24 @@
+"""merge heads
+
+Revision ID: 7d522e70629f
+Revises: 5f6e7d8c9b10, 9d0cb4e139bd
+Create Date: 2026-02-24 07:33:30.121336
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '7d522e70629f'
+down_revision = ('5f6e7d8c9b10', '9d0cb4e139bd')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
---
+++ migrations/versions/9d0cb4e139bd_merge_heads.py
@@ -0,0 +1,24 @@
+"""merge heads
+
+Revision ID: 9d0cb4e139bd
+Revises: 7ba83d22eece, f3c9b7a1d2e4
+Create Date: 2026-02-24 01:06:04.915817
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '9d0cb4e139bd'
+down_revision = ('7ba83d22eece', 'f3c9b7a1d2e4')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
