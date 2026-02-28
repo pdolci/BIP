f3c9550d0f26c453d617423a11bff18c37eb4faa
pdolci <149628511+pdolci@users.noreply.github.com>
2026-02-28 18:49:08 +0100

    Add CSRF protection to all POST forms

--- extensions.py
+++ extensions.py
@@ -2,8 +2,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
 from flask_migrate import Migrate
 from flask_limiter import Limiter
+from flask_wtf.csrf import CSRFProtect
 
 db = SQLAlchemy()
 mail = Mail()
 migrate = Migrate()
 limiter = Limiter()
+csrf = CSRFProtect()
