5963fa02f27239a3c45b6c99d135bcefc9b16388
pdolci <149628511+pdolci@users.noreply.github.com>
2026-03-01 19:03:50 +0100

    Limit homepage books to 9 random active titles

--- routes/public.py
+++ routes/public.py
@@ -1,4 +1,5 @@
 from flask import render_template
+from sqlalchemy.sql.expression import func
 
 from models import Book
 
@@ -8,6 +9,6 @@ from .core import _get_random_origin_quote
 
 @app_routes.route("/")
 def index():
-    books = Book.query.filter_by(is_active=True).order_by(Book.title.asc()).all()
+    books = Book.query.filter_by(is_active=True).order_by(func.random()).limit(9).all()
     random_quote = _get_random_origin_quote()
     return render_template("index.html", books=books, random_quote=random_quote)
